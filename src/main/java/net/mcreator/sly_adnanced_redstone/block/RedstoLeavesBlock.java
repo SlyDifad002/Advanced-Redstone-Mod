bbbc4e953a898bc585dc1f9dd372fa2ae2f9457e
Slyfalcosly <slyfalco.difad.com>
2021-03-02 16:26:01 +0000

    Fixed Trees

--- src/main/java/net/mcreator/sly_adnanced_redstone/block/RedstoLeavesBlock.java
+++ src/main/java/net/mcreator/sly_adnanced_redstone/block/RedstoLeavesBlock.java
@@ -12,7 +12,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
@@ -35,7 +34,7 @@ public class RedstoLeavesBlock extends SlyAdnancedRedstoneModElements.ModElement
 		elements.blocks.add(() -> new CustomBlock());
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(RWEItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-	public static class CustomBlock extends LeavesBlock {
+	public static class CustomBlock extends Block {
 		public CustomBlock() {
 			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(2f, 2f).lightValue(0));
 			setRegistryName("redsto_leaves");
